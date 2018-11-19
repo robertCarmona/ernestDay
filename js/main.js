cbff724d95d52182bba37699b60b77baca342009
Robert Carmona <robert100193@gmail.com>
2018-11-18 20:32:37 -0500

    using vue to shorten html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-console.log("connected");
-})
+Vue.component('merchandise',{
+  template: `
+    <div class = "col-3-lg col-4-md col-6-sm">
+  	 <img src = "img/merchandise.png">
+  	</div>`
+});
+
+new Vue({
+  el: "#merch",
+}
+
+
+)
