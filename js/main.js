4392078444e0fb9f52e9d3aedfb4701a3ef0d545
Robert Carmona <robert100193@gmail.com>
2018-11-18 21:56:15 -0500

    made mobile friencdly

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 Vue.component('merchandise',{
   template: `
-    <div class = "col-3-lg col-4-md col-6-sm">
+    <div class = "col-lg-3 col-md-4 col-sm-12 text-center">
   	 <img src = "img/merchandise.png">
+     <p>Name<p>
+     <p>165$</p>
+     <button>Buy</button>
   	</div>`
 });
 
 new Vue({
   el: "#merch",
+}
+
+
+)
+
+new Vue({
+  el: "#ernestNav",
 }
 
 
